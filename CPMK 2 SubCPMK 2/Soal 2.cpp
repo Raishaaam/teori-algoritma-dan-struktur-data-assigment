df5052e2b40e742ff12a66e9281f742a57035616
Raishaaam <raisha.amalia23@gmail.com>
2024-05-15 14:53:43 +0700

    CPMK 2 - SubCPMK2

--- CPMK 2 SubCPMK 2/Soal 2.cpp
+++ CPMK 2 SubCPMK 2/Soal 2.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
-#include <algorithm> // for std::swap
+#include <algorithm> 
 using namespace std;
 
-// Function to perform selection sort
 void selectionSort(int array[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
-        // Find the minimum element in the unsorted part of the array
         for (int j = i + 1; j < n; j++) {
             if (array[j] < array[minIndex]) {
                 minIndex = j;
             }
         }
-        // Swap the found minimum element with the first element of the unsorted part
+
         swap(array[i], array[minIndex]);
     }
 }
 
-// Function to print the array
 void printArray(int array[], int n) {
     for (int i = 0; i < n; i++) {
         cout << array[i] << " ";
@@ -26,14 +23,11 @@ void printArray(int array[], int n) {
 }
 
 int main() {
-    // Sample array to be sorted
     int array[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(array) / sizeof(array[0]);
 
     cout << "Array before sorting: ";
     printArray(array, n);
-
-    // Perform selection sort
     selectionSort(array, n);
 
     cout << "Array after sorting: ";
--- CPMK 2 SubCPMK 2/soal 3.cpp
+++ CPMK 2 SubCPMK 2/soal 3.cpp
@@ -6,11 +6,9 @@
 using namespace std;
 
 bool isAnagram(string str1, string str2) {
-    // Sorting both strings
     sort(str1.begin(), str1.end());
     sort(str2.begin(), str2.end());
 
-    // Comparing sorted strings
     if(str1 == str2) {
         return true;
     } else {
