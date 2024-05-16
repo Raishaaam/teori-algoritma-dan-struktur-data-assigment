5ed15566c7c61ad303066f23db9e2847e1e64b1a
Raishaaam <raisha.amalia23@gmail.com>
2024-05-16 23:11:27 +0700

    CPMK 2 - SubCPMK 4

---
+++ CPMK 2 SubCPMK 4/Soal 1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+// Mendefinisikan struktur bunga
+struct bunga {
+  string namaBunga;
+  string jenisBunga;
+  string warnaBunga;
+  int stokBunga;
+  string asalBunga;
+};
+
+int main() {
+  // Mendeklarasikan variabel favorit dengan tipe bunga
+  bunga favorit;
+
+  // Mengisi data ke dalam variabel favorit
+  favorit.namaBunga = "Lily";
+  favorit.jenisBunga = "Wood Lily";
+  favorit.warnaBunga = "Merah";
+  favorit. stokBunga = 34;
+  favorit.asalBunga = "Turki";
+
+  // Menampilkan informasi bunga favorit
+  cout << "\tBunga Favorit Saya" << endl;
+  cout << "\tNama Bunga : " << favorit.namaBunga << endl;
+  cout << "\tJenis Bunga : " << favorit.jenisBunga << endl;
+  cout << "\tWarna Bunga : " << favorit.warnaBunga << endl;
+  cout << "\tJumlah Bunga: " << favorit.stokBunga << " Bunga" << endl;
+  cout << "\tAsal Bunga : " << favorit.asalBunga << endl;
+
+  return 0;
+}
